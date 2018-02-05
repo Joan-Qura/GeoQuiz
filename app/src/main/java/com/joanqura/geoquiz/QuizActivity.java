3ece3d715a31dd276aae628db8ab2d7b6913928d
Qura-Joan <11140036@unmsm.edu.pe>
2018-02-05 15:46:42 -0500

    Inicio del Capítulo 4

--- app/src/main/java/com/joanqura/geoquiz/QuizActivity.java
+++ app/src/main/java/com/joanqura/geoquiz/QuizActivity.java
@@ -56,7 +56,7 @@ public class QuizActivity extends AppCompatActivity
         Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
-        mQuestionTextView = findViewById(R.id.question_text_view);
+       // mQuestionTextView = findViewById(R.id.question_text_view);
 
         mTrueButton = findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener()
